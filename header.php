a8bac82af206cde412c5e07de9b8f061bdd399cc
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-01-08 02:17:28 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,4 @@
+<header class="header">
+    <div class="header__logo" style="background: url(img/header/logo.png);"></div>
+    <i class="fas fa-bars"></i>
+</header>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../sass/css/style.css">
+    <link rel="stylesheet" href="../sass/css/null.css">
+    <link rel="stylesheet" href="../sass/css/root.css">
+    <script src="https://kit.fontawesome.com/673dec0f39.js" crossorigin="anonymous"></script>
+</head>
+<body class="body">
+    <div class="wrapper">
+        <?php require "header.php" ?>
+        <div class="card">
+            <h4 class="card__title"> Лаборатория робототехники БрГТУ </h4>
+            <p class="card__subtitle"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+    </p>
+        <span class="card__button"> Наша деятельность </span>
+        <div class="card__smedia">
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-telegram-plane"></i>
+        <i class="fab fa-youtube"></i>
+        </div>
+        </div>
+
+
+    </div>
+
+  
+
+    <script src="../script/script.js"></script>
+</body>
+</html>
