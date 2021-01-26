6fe7e1c9b7b8e23332ca84e7bb33d2e573e5cdb3
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-01-26 22:47:38 +0300

    0.0.7

--- index.php
+++ index.php
@@ -21,19 +21,26 @@
             <div class='card__descr'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
                 pretium quis, sem. Nulla consequat massa quis enim.
             </div>
-            <div class='card__button'>
-                Наша деятельность
+            <div class='card__wrapper'>
+                <div class='card__button'>
+                    Наша деятельность
+                </div>
+                <a class='card__history card__history_hidden'>
+                    История <i class="bi bi-chevron-right"></i>
+                </a>
             </div>
+
             <div class="card__media">
                 <i class="bi bi-facebook"></i>
                 <i class="bi bi-instagram"></i>
                 <i class="bi bi-telegram"></i>
                 <i class="bi bi-youtube"></i>
             </div>
-            <a class='card__history'>
+            <div class='aboba'>
+                <a class='card__history'>
                 История <i class="bi bi-chevron-right"></i>
             </a>
-
+            </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
