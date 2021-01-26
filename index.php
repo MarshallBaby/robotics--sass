b11b0d1484860195678228209606690d93a4ad9b
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-01-26 21:35:09 +0300

    0.0.6
    Сделана card mobile
    
    Co-Authored-By: VladsonX <75186030+VladsonX@users.noreply.github.com>

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Robotics</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="sass/css/style.css" rel="stylesheet">
 
 </head>
@@ -25,10 +26,13 @@
             </div>
             <div class="card__media">
                 <i class="bi bi-facebook"></i>
-                <i class="bi bi-facebook"></i>
-                <i class="bi bi-facebook"></i>
-                <i class="bi bi-facebook"></i>
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-telegram"></i>
+                <i class="bi bi-youtube"></i>
             </div>
+            <a class='card__history'>
+                История <i class="bi bi-chevron-right"></i>
+            </a>
 
         </div>
     </div>
