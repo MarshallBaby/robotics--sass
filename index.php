427a249529eb9875d0520b2cd0ce1917e60cdca3
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-01-26 21:10:33 +0300

    0.0.5
    Co-Authored-By: VladsonX <75186030+VladsonX@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Robotics</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="sass/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card__background"></div>
+            <h1 class='card__title'>
+                Лаборатория робототехники БрГТУ
+            </h1>
+            <div class='card__descr'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+                pretium quis, sem. Nulla consequat massa quis enim.
+            </div>
+            <div class='card__button'>
+                Наша деятельность
+            </div>
+            <div class="card__media">
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-facebook"></i>
+            </div>
+
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
