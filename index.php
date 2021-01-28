6870ec30b6166650eb9e3eb9b6d40ec40089bb1a
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-01-28 13:31:34 +0300

    0.0.14
    Полезные ссылки

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                     Наша деятельность
                 </div>
                 <a class='card__history card__history_hidden'>
-                История <i class="bi bi-chevron-right"></i>
+                    История <i class="bi bi-chevron-right"></i>
                 </a>
             </div>
 
@@ -38,8 +38,8 @@
             </div>
             <div class='aboba'>
                 <a class='card__history'>
-                История <i class="bi bi-chevron-right"></i>
-            </a>
+                    История <i class="bi bi-chevron-right"></i>
+                </a>
             </div>
         </section>
         <section class='news'>
@@ -47,59 +47,93 @@
                 <h2 class='news__title'>Новости</h2>
             </div>
             <div class='container d-flex flex-column align-items-center'>
-            <div class="row equal">
-            <div class="news__card col-xs-12 col-sm-6 col-lg-4">
-                    <div class='news__sticker'>
-                        <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
-                        <div class='news__help-container'>
-                        <h3 class='news__subtitle'>Второе место на WorldSkills СНГ</h3>
-                        <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
-                        <div class='news__more'>
-                            <a href="#">Подробнее</a>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="news__card col-xs-12 col-sm-6 col-lg-4">
-                    <div class='news__sticker'>
-                        <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
-                        <div class='news__help-container'>
-                        <h3 class='news__subtitle'>Чемпионы WorldSkills 2014</h3>
-                        <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
-                        <div class='news__more'>
-                            <a href="#">Подробнее</a>
-                        </div>
+                <div class="row equal">
+                    <div class="news__card col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Второе место на WorldSkills СНГ</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="news__card col-xs-12 col-sm-6 col-lg-4">
-                    <div class='news__sticker'>
-                        <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
-                        <div class='news__help-container'>
-                        <h3 class='news__subtitle'>Arduino Day 2014</h3>
-                        <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
-                        <div class='news__more'>
-                            <a href="#">Подробнее</a>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="news__card d-none d-sm-flex d-lg-none col-xs-12 col-sm-6 col-lg-4">
-                    <div class='news__sticker'>
-                        <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
-                        <div class='news__help-container'>
-                        <h3 class='news__subtitle'>Arduino Day 2014</h3>
-                        <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
-                        <div class='news__more'>
-                            <a href="#">Подробнее</a>
-                        </div>
+                    <div class="news__card col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Чемпионы WorldSkills 2014</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div class="news__card col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Arduino Day 2014</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__card d-none d-sm-flex d-lg-none col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Arduino Day 2014</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                
-</div>
-<div class='news__all-news'>
+                <div class='news__all-news'>
                     <a href="#">все новости</a>
+                </div>
+            </div>
+        </section>
+        <section class='links'>
+            <div class='container'>
+                <h2 class="links__title">Полезные ссылки</h2>
+                <div class="container">
+                    <div class='row'>
+                        <div class='col-sm-12 col-lg-4 col-xl-4 col-xxl-3'>
+                            <div class='links__wrapper'>
+                                <div class='links__item links__item_active'>
+                                    RoboRace
+                                </div>
+                                <div class='links__item'>
+                                    TowerLight Urcap
+                                </div>
+                                <div class='links__item'>
+                                    WIKI
+                                </div>
+                                <div class='links__item'>
+                                    RAIPAP
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class='offset-lg-1 col-lg-7 offset-xl-0 col-xl-6 col-xxl-6 d-none d-lg-flex'>
+                            <div class="links__info">
+                                <div class='links__subtitle'>RoboRace</div>
+                                <div class='Links__desrc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</div>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </section>
--- script/script.js
+++ script/script.js
@@ -1,19 +1,27 @@
 let fullWidther = (obj, limit) => {
     let sw = screen.width;
-    if(sw < limit){
+    if (sw < limit) {
         obj.style.width = sw + "px";
-    }else{
-        obj = limit = "px";
+    } else {
+        obj.style.width = limit + "px";
     }
 }
 
 function ready() {
     fullWidther(document.querySelectorAll(".news__all-news")[0], 1400);
-    
+
     window.addEventListener('resize', function(event) {
         fullWidther(document.querySelectorAll(".news__all-news")[0], 1400);
     });
-    console.log(typeof(document.querySelectorAll(".news__all-news")[0]));
+
+    links = document.getElementsByClassName('links__item');
+    for (let i = 0; i < links.length; i++) {
+        links[i].setAttribute('data-id', i);
+        links[i].addEventListener('hover')
+    }
+
+
+
 }
 
 document.addEventListener("DOMContentLoaded", ready);
